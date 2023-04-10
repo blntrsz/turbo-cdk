c89e65dac1c52cdb0eec991f35bdb00eb39592e9
Your Name <you@example.com>
2023-04-10 18:34:35 +0200

    add correct pipeline

--- apps/infra/lib/pipeline.ts
+++ apps/infra/lib/pipeline.ts
@@ -7,23 +7,38 @@ export class Pipeline extends Construct {
 
     new pipelines.CodePipeline(this, "Pipeline", {
       pipelineName: "MyPipeline",
-      synth: new pipelines.ShellStep("Synth", {
+      synth: new pipelines.CodeBuildStep("SynthStep", {
         input: pipelines.CodePipelineSource.connection(
           "blntrsz/turbo-cdk",
           "main",
           {
+            codeBuildCloneOutput: true,
             connectionArn:
               "arn:aws:codestar-connections:eu-central-1:155601209279:connection/51dc3226-ffdd-459d-a034-50f9ff503d2b",
           }
         ),
-        commands: [
-          "node -v",
-          "npm -v",
-          "npm i -g pnpm@7.30.3",
-          "pnpm i",
-          "pnpm infra:deploy",
-        ],
+        installCommands: ["npm install -g aws-cdk pnpm@7.30.3"],
+        commands: ["git status", "pnpm i", "pnpm infra:deploy"],
       }),
+      // new pipelines.ShellStep("Synth", {
+      // input: pipelines.CodePipelineSource.connection(
+      //   "blntrsz/turbo-cdk",
+      //   "main",
+      //   {
+      //     connectionArn:
+      //       "arn:aws:codestar-connections:eu-central-1:155601209279:connection/51dc3226-ffdd-459d-a034-50f9ff503d2b",
+      //   }
+      // ),
+      // commands: [
+      //   "ls -la",
+      //   "git status",
+      //   "node -v",
+      //   "npm -v",
+      //   "npm i -g pnpm@7.30.3",
+      //   "pnpm i",
+      //   "pnpm infra:deploy",
+      // ],
+      // }),
     });
   }
 }

98a6f7ac02352f0b071746ec8caf188ad83bcef5
Your Name <you@example.com>
2023-04-10 18:12:25 +0200

    add pipeline stack

---
+++ apps/infra/lib/pipeline.ts
@@ -0,0 +1,23 @@
+import * as pipelines from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+
+export class Pipeline extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new pipelines.CodePipeline(this, "Pipeline", {
+      pipelineName: "MyPipeline",
+      synth: new pipelines.ShellStep("Synth", {
+        input: pipelines.CodePipelineSource.connection(
+          "blntrsz/turbo-cdk",
+          "main",
+          {
+            connectionArn:
+              "arn:aws:codestar-connections:eu-central-1:155601209279:connection/51dc3226-ffdd-459d-a034-50f9ff503d2b",
+          }
+        ),
+        commands: ["npm i -g pnpm", "pnpm i", "ls -la", "pnpm infra:deploy"],
+      }),
+    });
+  }
+}

7609f110b07e6c0decebfefc2087fb42621a000c
Your Name <you@example.com>
2023-04-10 18:12:00 +0200

    add pipeline stack

--- apps/infra/lib/infra-stack.ts
+++ apps/infra/lib/infra-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Backend } from "./backend";
 import { Construct } from "constructs";
 import { Frontend } from "./frontend";
+import { Pipeline } from "./pipeline";
 
 export class InfraStack extends cdk.Stack {
   constructor(
@@ -12,6 +13,7 @@ export class InfraStack extends cdk.Stack {
   ) {
     super(scope, id, props);
 
+    new Pipeline(this, "pipeline");
     new Backend(this, "backend");
     new Frontend(this, "frontend", stage);
   }
